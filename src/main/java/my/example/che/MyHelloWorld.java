b128db848d663f587ca4e7c75822397325eaea2c
Anuradha Raghunathan <araghunatha@@csc.com>
2017-06-06 19:52:12 +0000

    my secnd one

---
+++ src/main/java/my/example/che/MyHelloWorld.java
@@ -0,0 +1,10 @@
+package my.example.che;
+
+public class MyHelloWorld {
+    public static void main(String... argsv){
+        String a = "Che";
+        System.out.println("Hello World This is " + a + "!!" );
+        
+    }
+}
+
