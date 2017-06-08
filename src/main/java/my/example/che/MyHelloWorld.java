ad35e101dbf485909616b84a2b1173bb12d13da2
Anuradha Raghunathan <araghunatha@@csc.com>
2017-06-08 06:47:16 +0000

    test message

--- src/main/java/my/example/che/MyHelloWorld.java
+++ src/main/java/my/example/che/MyHelloWorld.java
@@ -2,8 +2,9 @@ package my.example.che;
 
 public class MyHelloWorld {
     public static void main(String... argsv){
-        String a = "Che";
+        String a = "Che1";
         System.out.println("Hello World This is " + a + "!!" );
+        
         
     }
 }
